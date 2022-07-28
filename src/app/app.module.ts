c34b8dd7dd2c428c942136d50ae1514b0428ec74
Dmitriy <dgdagadin87@mail.ru>
2022-07-28 19:29:35 +0300

    Project was modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatSliderModule } from '@angular/material/slider';
+import { HttpClientModule } from '@angular/common/http';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table';
 import { MoneyListComponent } from './components/money-list/money-list.component';
 
 @NgModule({
@@ -14,7 +17,10 @@ import { MoneyListComponent } from './components/money-list/money-list.component
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatSliderModule,
+    HttpClientModule,
+    MatCardModule,
+    MatButtonModule,
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
